ee3afa4a37ff65708291168fa2cf879c30775a51
alibaltaci <alibaltaci13@gmail.com>
2022-04-03 08:33:54 +0300

    firts commit

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Bu benim uygulamam...");
+
+// Şimdi bu kodu çalıştırmak için script'i package.json 'a ekleyelim.
+
+// "start": "node index.js" - şeklinde
+
+// Çlaıştırmak için
+
+// npm run start
+
+// -------------------------
+
+// Paket ekleme
+
+// npm install simple-is
+
+// dependencies - kısmına eklenir
+
+// "dependencies": {
+//     "simple-is": "^0.2.0"
+//   }
